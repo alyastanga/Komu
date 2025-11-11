fbf36208824ef2d99821a8159a1ea7c3927ba0cf
Peter <peterdelacruz827@gmail.com>
2025-11-11 17:46:24 +0800

    refactor readme

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT=$( dirname "$SCRIPT_DIR" )
 
 BUILD_DIR="${PROJECT_ROOT}/build"
+EXAMPLES_DIR="${PROJECT_ROOT}/examples"
 
 echo "onfiguring Project..."
 cmake -B "$BUILD_DIR" -S "$PROJECT_ROOT"
@@ -16,9 +17,14 @@ echo "Running Tests..."
 ctest --test-dir "$BUILD_DIR" --output-on-failure
 echo "All tests passed!"
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <file.komu>"
+  exit 1
+fi
+
 echo "Running parser..."
 cd "$PROJECT_ROOT"
-python3 -m src.parser.src.main
+python3 -m src.parser.src.main "${EXAMPLES_DIR}/$1"
 
 
 echo "Running interpreter..."
